d89805ed8c326cf8e31502db2de2a166e8e99846
Cristian G Espinosa <107425906+Quishy08@users.noreply.github.com>
2025-01-28 19:33:11 +0000

    changed the body and added a new image

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Cristian G Espinosa",
+  title: "Web Designer & Developer",
+  email: "CristianGEO804@gmail.com",
+  gitHub: "Quishy08",
+  instagram: "cristianespinosa___",
+  linkedIn: "crisgeo08",
 };
 
 const primaryColor = "#4E567E";
